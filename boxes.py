045fa122c7a64aead15ac446c46a222d57bee308
YurikO0328 <aileencalimutan@gmail.com>
2025-06-30 19:00:51 +0100

    Add files via upload

---
+++ boxes.py
@@ -0,0 +1,26 @@
+import pgzrun
+import pygame
+import random 
+
+WIDTH=500
+HEIGHT=500
+
+def draw():
+    for i in range(20):
+        width=WIDTH-100
+        height=HEIGHT-150
+        r=255
+        g=0
+        b=100
+        rect.center
+        rect=Rect((0,0)(width,height))
+        screen.draw.rect(rect,(r,g,b))
+
+    width=width-10
+    height=height-15
+    
+
+
+
+
+pgzrun.go()
